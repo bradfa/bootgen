bradfa/bootgen

commit efa41c591195a0ffa86528e391bcd01ff76aded0

2026.1 feature update  (#53)
Features:
- 2026.1 updates for LMS, HSS and HSM authentication
- Telluride HSM authJtag support
- Telluride  fixes 
Bug fixes:
-  pcr index not being picked up by telluride arch
- bbram_aes_red_keyrolling_with_wrong_block_size_test
- versal and CDO sequence updates

common/src/authentication.cpp
@@ -205,13 +205,27 @@ AuthHash::Type AuthenticationContext::GetHashType(void)
 }
 
 /******************************************************************************/
+void AuthenticationContext::WritePaddedSHAFile(const uint8_t* buffer, size_t bufferLen, const std::string& hashfilename)
+{
+    std::string filename =  StringUtils::BaseName(hashfilename);
+    std::ofstream f(filename.c_str(),std::ios_base::out|std::ios_base::binary);
+    f.write((char*)buffer, bufferLen);
+    f.close();
+
+    if (f.fail())
+    {
+        LOG_ERROR("-generate_hashes failure !!!\n           Failed to write generated hash to the file: %s", filename.c_str());
+    }
+    LOG_TRACE("Hash file %s generated successfully", filename.c_str());
+}
+
+/******************************************************************************/
 void AuthenticationContext::WritePaddedSHAFile(const uint8_t* shaBuf, const std::string& hashfilename)
 {
     std::string filename =  StringUtils::BaseName(hashfilename);
     std::ofstream f(filename.c_str(),std::ios_base::out|std::ios_base::binary);
     f.write((char*)shaBuf, signatureLength);
     f.close();
-
     if (f.fail())
     {
         LOG_ERROR("-generate_hashes failure !!!\n           Failed to write generated hash to the file: %s", filename.c_str());
@@ -279,6 +293,7 @@ void AuthenticationContext::GetPresign(const std::string& presignFilename, uint8
     }
 
     LOG_TRACE("Reading presign file - %s", filename.c_str());
+    signature = new uint8_t[signatureLength];
     FILE* filePtr;
     filePtr = fopen(filename.c_str(),"rb");
     if (filePtr) 

bisonflex/cmdoptions.tab.hpp
@@ -131,168 +131,187 @@ namespace CO {
      _PUF = 284,
      DUMP_SLAVE_PDIS = 285,
      DUMP_PUF_PDI = 286,
-     _ARCH = 287,
-     ZYNQ = 288,
-     ZYNQMP = 289,
-     VERSAL = 290,
-     _R = 291,
-     FPGA = 292,
-     VERSALNET = 293,
-     TELLURIDE = 294,
-     VERSAL_2VE_2VM = 295,
-     LASSEN = 296,
-     LASSEN_DL9 = 297,
-     SPARTANUP = 298,
-     _DUAL_QSPI_MODE = 299,
-     _DUAL_OSPI_MODE = 300,
-     PARALLEL = 301,
-     STACKED = 302,
-     _W = 303,
-     ON = 304,
-     OFF = 305,
-     _NOAUTHBLOCKS = 306,
-     _ZYNQMPES1 = 307,
-     _OVERLAYCDO = 308,
-     _EFUSEPPKBITS = 309,
-     _GENERATE_HASHES = 310,
-     _PADIMAGEHEADER = 311,
-     _SPKSIGNATURE = 312,
-     _GENERATE_KEYS = 313,
-     PEM = 314,
-     RSA = 315,
-     ECDSAP521 = 316,
-     AUTH = 317,
-     GREY = 318,
-     METAL = 319,
-     LMS = 320,
-     _EFUSEPUFBITS = 321,
-     _SECUREDEBUG = 322,
-     ECDSA = 323,
-     _AUTHJTAG = 324,
-     _ENCRYPT = 325,
-     BBRAM = 326,
-     EFUSE = 327,
-     _P_TOK = 328,
-     _READ = 329,
-     READ_BH = 330,
-     READ_IHT = 331,
-     READ_IH = 332,
-     READ_PHT = 333,
-     READ_AC = 334,
-     _VERIFY = 335,
-     _VERIFYKDF = 336,
-     _AUTH_OPTIMIZATION = 337,
-     _ZYNQMPENCRDUMP = 338,
-     HEXVALUE = 339,
-     IDENTIFIER = 340,
-     FILENAME = 341,
-     QFILENAME = 342,
-     HEXSTRING = 343,
-     EQUALS = 344,
-     HMAC = 345,
-     STARTCBC = 346,
-     KEY0 = 347,
-     COMMA = 348,
-     HBIFHELP = 349,
-     HARCH = 350,
-     HIMAGE = 351,
-     HFILL = 352,
-     HO = 353,
-     HP = 354,
-     HW = 355,
-     HEFUSEPPKBITS = 356,
-     HGENHASHES = 357,
-     HLEGACY = 358,
-     HPADHDR = 359,
-     H_SPKSIGN = 360,
-     HAUTHOPT = 361,
-     HPACKAGE = 362,
-     HENCRYPT = 363,
-     HGENKEYS = 364,
-     HDQSPI = 365,
-     HLOG = 366,
-     HZYNQMPES1 = 367,
-     HPROCESSBIT = 368,
-     HNONBOOTING = 369,
-     HENCRDUMP = 370,
-     HVERIFY = 371,
-     HSECUREDEBUG = 372,
-     HREAD = 373,
-     HVERIFYKDF = 374,
-     HDUMP = 375,
-     HDUMPDIR = 376,
-     HOVLCDO = 377,
-     HOUTTYPE = 378,
-     H_BIF_INIT = 379,
-     H_BIF_UDFBH = 380,
-     H_BIF_AES = 381,
-     H_BIF_PPK = 382,
-     H_BIF_PSK = 383,
-     H_BIF_SPK = 384,
-     H_BIF_SSK = 385,
-     H_BIF_SPKSIGN = 386,
-     H_BIF_HIVEC = 387,
-     H_BIF_HDRSIGN = 388,
-     H_BIF_BOOTIMAGE = 389,
-     H_BIF_BL = 390,
-     H_BIF_PID = 391,
-     H_BIF_ENCR = 392,
-     H_BIF_AUTH = 393,
-     H_BIF_CHKSM = 394,
-     H_BIF_ELYHNDOFF = 395,
-     H_BIF_BHSIGN = 396,
-     H_BIF_TCMBOOT = 397,
-     H_BIF_OPTIONALDATA = 398,
-     H_BIF_POWNER = 399,
-     H_BIF_PRESIGN = 400,
-     H_BIF_UDF = 401,
-     H_BIF_XIP = 402,
-     H_BIF_ALIGN = 403,
-     H_BIF_OFFSET = 404,
-     H_BIF_RES = 405,
-     H_BIF_LOAD = 406,
-     H_BIF_TZ = 407,
-     H_BIF_STARTUP = 408,
-     H_BIF_KEYSRC = 409,
-     H_BIF_FSBLCFG = 410,
-     H_BIF_BOOTDEV = 411,
-     H_BIF_DESTCPU = 412,
-     H_BIF_DESTDEV = 413,
-     H_BIF_EL = 414,
-     H_SPLIT = 415,
-     H_BIF_AUTHPARAM = 416,
-     H_BIF_BHKEY = 417,
-     H_BIF_PFW = 418,
-     H_BIF_BLOCKS = 419,
-     H_BIF_METAL = 420,
-     H_BIF_BHIV = 421,
-     H_BIF_BOOTVEC = 422,
-     H_BIF_PUFDATA = 423,
-     H_BIF_PTYPE = 424,
-     H_BIF_IMAGECFG = 425,
-     H_BIF_PMCCONFIG = 426,
-     H_BIF_AARCH32 = 427,
-     H_BIF_BIGENDIAN = 428,
-     H_BIF_BOOTCONFIG = 429,
-     H_BIF_COPY = 430,
-     H_BIF_CORE = 431,
-     H_BIF_DELAY_HANDOFF = 432,
-     H_BIF_DELAY_LOAD = 433,
-     H_BIF_FILE = 434,
-     H_BIF_ID = 435,
-     H_BIF_IMAGE = 436,
-     H_BIF_METAHDR = 437,
-     H_BIF_NAME = 438,
-     H_BIF_PARTITION = 439,
-     H_BIF_SLR = 440,
-     H_BIF_TYPE = 441,
-     H_BIF_KEYSRCENCR = 442,
-     H_BIF_PARENTID = 443,
-     H_DPACM_ENABLE = 444,
-     H_BIF_USERKEYS = 445,
-     HVN_BIF_PCR = 446,
-     HVN_BIF_PCR_MINDEX = 447,
-     HV_BIF_IMAGESTORE = 448
+     _SYNCFLAG = 287,
+     _ARCH = 288,
+     ZYNQ = 289,
+     ZYNQMP = 290,
+     VERSAL = 291,
+     _R = 292,
+     FPGA = 293,
+     VERSALNET = 294,
+     TELLURIDE = 295,
+     VERSAL_2VE_2VM = 296,
+     LASSEN = 297,
+     LASSEN_DL9 = 298,
+     SPARTANUP = 299,
+     LASSEN_DL13 = 300,
+     VERSAL_2VP = 301,
+     _DUAL_QSPI_MODE = 302,
+     _DUAL_OSPI_MODE = 303,
+     PARALLEL = 304,
+     STACKED = 305,
+     _W = 306,
+     ON = 307,
+     OFF = 308,
+     _NOAUTHBLOCKS = 309,
+     _ZYNQMPES1 = 310,
+     _POST_PROCESS = 311,
+     _OVERLAYCDO = 312,
+     _ENABLE_SUBSYSTEMS = 313,
+     _EFUSEPPKBITS = 314,
+     _GENERATE_HASHES = 315,
+     _PADIMAGEHEADER = 316,
+     _SPKSIGNATURE = 317,
+     _GENERATE_KEYS = 318,
+     PEM = 319,
+     RSA = 320,
+     ECDSAP521 = 321,
+     AUTH = 322,
+     GREY = 323,
+     METAL = 324,
+     LMS = 325,
+     _EFUSEPUFBITS = 326,
+     _SECUREDEBUG = 327,
+     ECDSA = 328,
+     _AUTHJTAG = 329,
+     LMS_SHA256 = 330,
+     LMS_SHAKE256 = 331,
+     HSS_SHA256 = 332,
+     HSS_SHAKE256 = 333,
+     _ENCRYPT = 334,
+     BBRAM = 335,
+     EFUSE = 336,
+     _P_TOK = 337,
+     _INTERFACE = 338,
+     SMAPx8 = 339,
+     SMAPx16 = 340,
+     SMAPx32 = 341,
+     SPI = 342,
+     BPIx8 = 343,
+     BPIx16 = 344,
+     _READ = 345,
+     READ_BH = 346,
+     READ_IHT = 347,
+     READ_IH = 348,
+     READ_PHT = 349,
+     READ_AC = 350,
+     _VERIFY = 351,
+     _VERIFYKDF = 352,
+     _BIF_TO_JSON = 353,
+     _AUTH_OPTIMIZATION = 354,
+     _ZYNQMPENCRDUMP = 355,
+     HEXVALUE = 356,
+     IDENTIFIER = 357,
+     FILENAME = 358,
+     QFILENAME = 359,
+     HEXSTRING = 360,
+     EQUALS = 361,
+     HMAC = 362,
+     STARTCBC = 363,
+     KEY0 = 364,
+     COMMA = 365,
+     HBIFHELP = 366,
+     HARCH = 367,
+     HIMAGE = 368,
+     HFILL = 369,
+     HO = 370,
+     HP = 371,
+     HW = 372,
+     HEFUSEPPKBITS = 373,
+     HGENHASHES = 374,
+     HLEGACY = 375,
+     HPADHDR = 376,
+     H_SPKSIGN = 377,
+     HAUTHOPT = 378,
+     HPACKAGE = 379,
+     HENCRYPT = 380,
+     HGENKEYS = 381,
+     HDQSPI = 382,
+     HLOG = 383,
+     HZYNQMPES1 = 384,
+     HPROCESSBIT = 385,
+     HNONBOOTING = 386,
+     HENCRDUMP = 387,
+     HPOSTPROCESS = 388,
+     HVERIFY = 389,
+     HSECUREDEBUG = 390,
+     HREAD = 391,
+     HVERIFYKDF = 392,
+     HDUMP = 393,
+     HDUMPDIR = 394,
+     HOVLCDO = 395,
+     HOUTTYPE = 396,
+     H_BIF_INIT = 397,
+     H_BIF_UDFBH = 398,
+     H_BIF_AES = 399,
+     H_BIF_PPK = 400,
+     H_BIF_PSK = 401,
+     H_BIF_SPK = 402,
+     H_BIF_SSK = 403,
+     H_BIF_SPKSIGN = 404,
+     H_BIF_HIVEC = 405,
+     H_BIF_HDRSIGN = 406,
+     H_BIF_BOOTIMAGE = 407,
+     H_BIF_BL = 408,
+     H_BIF_PLDATA = 409,
+     H_BIF_PID = 410,
+     H_BIF_ENCR = 411,
+     H_BIF_AUTH = 412,
+     H_BIF_CHKSM = 413,
+     H_BIF_ELYHNDOFF = 414,
+     H_BIF_BHSIGN = 415,
+     H_BIF_TCMBOOT = 416,
+     H_BIF_OPTIONALDATA = 417,
+     H_BIF_POWNER = 418,
+     H_BIF_PRESIGN = 419,
+     H_BIF_UDF = 420,
+     H_BIF_XIP = 421,
+     H_BIF_ALIGN = 422,
+     H_BIF_OFFSET = 423,
+     H_BIF_RES = 424,
+     H_BIF_LOAD = 425,
+     H_BIF_TZ = 426,
+     H_BIF_STARTUP = 427,
+     H_BIF_KEYSRC = 428,
+     H_BIF_FSBLCFG = 429,
+     H_BIF_BOOTDEV = 430,
+     H_BIF_DESTCPU = 431,
+     H_BIF_DESTDEV = 432,
+     H_BIF_EL = 433,
+     H_SPLIT = 434,
+     H_BIF_AUTHPARAM = 435,
+     H_BIF_BHKEY = 436,
+     H_BIF_PFW = 437,
+     H_BIF_BLOCKS = 438,
+     H_BIF_METAL = 439,
+     H_BIF_BHIV = 440,
+     H_BIF_BOOTVEC = 441,
+     H_BIF_PUFDATA = 442,
+     H_BIF_PTYPE = 443,
+     H_BIF_IMAGECFG = 444,
+     H_BIF_PMCCONFIG = 445,
+     H_BIF_AARCH32 = 446,
+     H_BIF_BIGENDIAN = 447,
+     H_BIF_BOOTCONFIG = 448,
+     H_BIF_COPY = 449,
+     H_BIF_CORE = 450,
+     H_BIF_DELAY_HANDOFF = 451,
+     H_BIF_DELAY_LOAD = 452,
+     H_BIF_FILE = 453,
+     H_BIF_ID = 454,
+     H_BIF_IMAGE = 455,
+     H_BIF_METAHDR = 456,
+     H_BIF_NAME = 457,
+     H_BIF_PARTITION = 458,
+     H_BIF_SLR = 459,
+     H_BIF_TYPE = 460,
+     H_BIF_KEYSRCENCR = 461,
+     H_BIF_PARENTID = 462,
+     H_DPACM_ENABLE = 463,
+     H_BIF_USERKEYS = 464,
+     HVN_BIF_PCR = 465,
+     HVN_BIF_PCR_MINDEX = 466,
+     HV_BIF_IMAGESTORE = 467
    };
 
     };
@@ -469,7 +488,7 @@ namespace CO {
 #line 24 "../../parser/cmdoptions.y"
 } // CO
 /* Line 33 of lalr1.cc  */
-#line 473 "cmdoptions.tab.hpp"
+#line 492 "cmdoptions.tab.hpp"
 
 
 

bisonflex/cmdoptions/cmdoptions.tab.hpp
@@ -131,168 +131,187 @@ namespace CO {
      _PUF = 284,
      DUMP_SLAVE_PDIS = 285,
      DUMP_PUF_PDI = 286,
-     _ARCH = 287,
-     ZYNQ = 288,
-     ZYNQMP = 289,
-     VERSAL = 290,
-     _R = 291,
-     FPGA = 292,
-     VERSALNET = 293,
-     TELLURIDE = 294,
-     VERSAL_2VE_2VM = 295,
-     LASSEN = 296,
-     LASSEN_DL9 = 297,
-     SPARTANUP = 298,
-     _DUAL_QSPI_MODE = 299,
-     _DUAL_OSPI_MODE = 300,
-     PARALLEL = 301,
-     STACKED = 302,
-     _W = 303,
-     ON = 304,
-     OFF = 305,
-     _NOAUTHBLOCKS = 306,
-     _ZYNQMPES1 = 307,
-     _OVERLAYCDO = 308,
-     _EFUSEPPKBITS = 309,
-     _GENERATE_HASHES = 310,
-     _PADIMAGEHEADER = 311,
-     _SPKSIGNATURE = 312,
-     _GENERATE_KEYS = 313,
-     PEM = 314,
-     RSA = 315,
-     ECDSAP521 = 316,
-     AUTH = 317,
-     GREY = 318,
-     METAL = 319,
-     LMS = 320,
-     _EFUSEPUFBITS = 321,
-     _SECUREDEBUG = 322,
-     ECDSA = 323,
-     _AUTHJTAG = 324,
-     _ENCRYPT = 325,
-     BBRAM = 326,
-     EFUSE = 327,
-     _P_TOK = 328,
-     _READ = 329,
-     READ_BH = 330,
-     READ_IHT = 331,
-     READ_IH = 332,
-     READ_PHT = 333,
-     READ_AC = 334,
-     _VERIFY = 335,
-     _VERIFYKDF = 336,
-     _AUTH_OPTIMIZATION = 337,
-     _ZYNQMPENCRDUMP = 338,
-     HEXVALUE = 339,
-     IDENTIFIER = 340,
-     FILENAME = 341,
-     QFILENAME = 342,
-     HEXSTRING = 343,
-     EQUALS = 344,
-     HMAC = 345,
-     STARTCBC = 346,
-     KEY0 = 347,
-     COMMA = 348,
-     HBIFHELP = 349,
-     HARCH = 350,
-     HIMAGE = 351,
-     HFILL = 352,
-     HO = 353,
-     HP = 354,
-     HW = 355,
-     HEFUSEPPKBITS = 356,
-     HGENHASHES = 357,
-     HLEGACY = 358,
-     HPADHDR = 359,
-     H_SPKSIGN = 360,
-     HAUTHOPT = 361,
-     HPACKAGE = 362,
-     HENCRYPT = 363,
-     HGENKEYS = 364,
-     HDQSPI = 365,
-     HLOG = 366,
-     HZYNQMPES1 = 367,
-     HPROCESSBIT = 368,
-     HNONBOOTING = 369,
-     HENCRDUMP = 370,
-     HVERIFY = 371,
-     HSECUREDEBUG = 372,
-     HREAD = 373,
-     HVERIFYKDF = 374,
-     HDUMP = 375,
-     HDUMPDIR = 376,
-     HOVLCDO = 377,
-     HOUTTYPE = 378,
-     H_BIF_INIT = 379,
-     H_BIF_UDFBH = 380,
-     H_BIF_AES = 381,
-     H_BIF_PPK = 382,
-     H_BIF_PSK = 383,
-     H_BIF_SPK = 384,
-     H_BIF_SSK = 385,
-     H_BIF_SPKSIGN = 386,
-     H_BIF_HIVEC = 387,
-     H_BIF_HDRSIGN = 388,
-     H_BIF_BOOTIMAGE = 389,
-     H_BIF_BL = 390,
-     H_BIF_PID = 391,
-     H_BIF_ENCR = 392,
-     H_BIF_AUTH = 393,
-     H_BIF_CHKSM = 394,
-     H_BIF_ELYHNDOFF = 395,
-     H_BIF_BHSIGN = 396,
-     H_BIF_TCMBOOT = 397,
-     H_BIF_OPTIONALDATA = 398,
-     H_BIF_POWNER = 399,
-     H_BIF_PRESIGN = 400,
-     H_BIF_UDF = 401,
-     H_BIF_XIP = 402,
-     H_BIF_ALIGN = 403,
-     H_BIF_OFFSET = 404,
-     H_BIF_RES = 405,
-     H_BIF_LOAD = 406,
-     H_BIF_TZ = 407,
-     H_BIF_STARTUP = 408,
-     H_BIF_KEYSRC = 409,
-     H_BIF_FSBLCFG = 410,
-     H_BIF_BOOTDEV = 411,
-     H_BIF_DESTCPU = 412,
-     H_BIF_DESTDEV = 413,
-     H_BIF_EL = 414,
-     H_SPLIT = 415,
-     H_BIF_AUTHPARAM = 416,
-     H_BIF_BHKEY = 417,
-     H_BIF_PFW = 418,
-     H_BIF_BLOCKS = 419,
-     H_BIF_METAL = 420,
-     H_BIF_BHIV = 421,
-     H_BIF_BOOTVEC = 422,
-     H_BIF_PUFDATA = 423,
-     H_BIF_PTYPE = 424,
-     H_BIF_IMAGECFG = 425,
-     H_BIF_PMCCONFIG = 426,
-     H_BIF_AARCH32 = 427,
-     H_BIF_BIGENDIAN = 428,
-     H_BIF_BOOTCONFIG = 429,
-     H_BIF_COPY = 430,
-     H_BIF_CORE = 431,
-     H_BIF_DELAY_HANDOFF = 432,
-     H_BIF_DELAY_LOAD = 433,
-     H_BIF_FILE = 434,
-     H_BIF_ID = 435,
-     H_BIF_IMAGE = 436,
-     H_BIF_METAHDR = 437,
-     H_BIF_NAME = 438,
-     H_BIF_PARTITION = 439,
-     H_BIF_SLR = 440,
-     H_BIF_TYPE = 441,
-     H_BIF_KEYSRCENCR = 442,
-     H_BIF_PARENTID = 443,
-     H_DPACM_ENABLE = 444,
-     H_BIF_USERKEYS = 445,
-     HVN_BIF_PCR = 446,
-     HVN_BIF_PCR_MINDEX = 447,
-     HV_BIF_IMAGESTORE = 448
+     _SYNCFLAG = 287,
+     _ARCH = 288,
+     ZYNQ = 289,
+     ZYNQMP = 290,
+     VERSAL = 291,
+     _R = 292,
+     FPGA = 293,
+     VERSALNET = 294,
+     TELLURIDE = 295,
+     VERSAL_2VE_2VM = 296,
+     LASSEN = 297,
+     LASSEN_DL9 = 298,
+     SPARTANUP = 299,
+     LASSEN_DL13 = 300,
+     VERSAL_2VP = 301,
+     _DUAL_QSPI_MODE = 302,
+     _DUAL_OSPI_MODE = 303,
+     PARALLEL = 304,
+     STACKED = 305,
+     _W = 306,
+     ON = 307,
+     OFF = 308,
+     _NOAUTHBLOCKS = 309,
+     _ZYNQMPES1 = 310,
+     _POST_PROCESS = 311,
+     _OVERLAYCDO = 312,
+     _ENABLE_SUBSYSTEMS = 313,
+     _EFUSEPPKBITS = 314,
+     _GENERATE_HASHES = 315,
+     _PADIMAGEHEADER = 316,
+     _SPKSIGNATURE = 317,
+     _GENERATE_KEYS = 318,
+     PEM = 319,
+     RSA = 320,
+     ECDSAP521 = 321,
+     AUTH = 322,
+     GREY = 323,
+     METAL = 324,
+     LMS = 325,
+     _EFUSEPUFBITS = 326,
+     _SECUREDEBUG = 327,
+     ECDSA = 328,
+     _AUTHJTAG = 329,
+     LMS_SHA256 = 330,
+     LMS_SHAKE256 = 331,
+     HSS_SHA256 = 332,
+     HSS_SHAKE256 = 333,
+     _ENCRYPT = 334,
+     BBRAM = 335,
+     EFUSE = 336,
+     _P_TOK = 337,
+     _INTERFACE = 338,
+     SMAPx8 = 339,
+     SMAPx16 = 340,
+     SMAPx32 = 341,
+     SPI = 342,
+     BPIx8 = 343,
+     BPIx16 = 344,
+     _READ = 345,
+     READ_BH = 346,
+     READ_IHT = 347,
+     READ_IH = 348,
+     READ_PHT = 349,
+     READ_AC = 350,
+     _VERIFY = 351,
+     _VERIFYKDF = 352,
+     _BIF_TO_JSON = 353,
+     _AUTH_OPTIMIZATION = 354,
+     _ZYNQMPENCRDUMP = 355,
+     HEXVALUE = 356,
+     IDENTIFIER = 357,
+     FILENAME = 358,
+     QFILENAME = 359,
+     HEXSTRING = 360,
+     EQUALS = 361,
+     HMAC = 362,
+     STARTCBC = 363,
+     KEY0 = 364,
+     COMMA = 365,
+     HBIFHELP = 366,
+     HARCH = 367,
+     HIMAGE = 368,
+     HFILL = 369,
+     HO = 370,
+     HP = 371,
+     HW = 372,
+     HEFUSEPPKBITS = 373,
+     HGENHASHES = 374,
+     HLEGACY = 375,
+     HPADHDR = 376,
+     H_SPKSIGN = 377,
+     HAUTHOPT = 378,
+     HPACKAGE = 379,
+     HENCRYPT = 380,
+     HGENKEYS = 381,
+     HDQSPI = 382,
+     HLOG = 383,
+     HZYNQMPES1 = 384,
+     HPROCESSBIT = 385,
+     HNONBOOTING = 386,
+     HENCRDUMP = 387,
+     HPOSTPROCESS = 388,
+     HVERIFY = 389,
+     HSECUREDEBUG = 390,
+     HREAD = 391,
+     HVERIFYKDF = 392,
+     HDUMP = 393,
+     HDUMPDIR = 394,
+     HOVLCDO = 395,
+     HOUTTYPE = 396,
+     H_BIF_INIT = 397,
+     H_BIF_UDFBH = 398,
+     H_BIF_AES = 399,
+     H_BIF_PPK = 400,
+     H_BIF_PSK = 401,
+     H_BIF_SPK = 402,
+     H_BIF_SSK = 403,
+     H_BIF_SPKSIGN = 404,
+     H_BIF_HIVEC = 405,
+     H_BIF_HDRSIGN = 406,
+     H_BIF_BOOTIMAGE = 407,
+     H_BIF_BL = 408,
+     H_BIF_PLDATA = 409,
+     H_BIF_PID = 410,
+     H_BIF_ENCR = 411,
+     H_BIF_AUTH = 412,
+     H_BIF_CHKSM = 413,
+     H_BIF_ELYHNDOFF = 414,
+     H_BIF_BHSIGN = 415,
+     H_BIF_TCMBOOT = 416,
+     H_BIF_OPTIONALDATA = 417,
+     H_BIF_POWNER = 418,
+     H_BIF_PRESIGN = 419,
+     H_BIF_UDF = 420,
+     H_BIF_XIP = 421,
+     H_BIF_ALIGN = 422,
+     H_BIF_OFFSET = 423,
+     H_BIF_RES = 424,
+     H_BIF_LOAD = 425,
+     H_BIF_TZ = 426,
+     H_BIF_STARTUP = 427,
+     H_BIF_KEYSRC = 428,
+     H_BIF_FSBLCFG = 429,
+     H_BIF_BOOTDEV = 430,
+     H_BIF_DESTCPU = 431,
+     H_BIF_DESTDEV = 432,
+     H_BIF_EL = 433,
+     H_SPLIT = 434,
+     H_BIF_AUTHPARAM = 435,
+     H_BIF_BHKEY = 436,
+     H_BIF_PFW = 437,
+     H_BIF_BLOCKS = 438,
+     H_BIF_METAL = 439,
+     H_BIF_BHIV = 440,
+     H_BIF_BOOTVEC = 441,
+     H_BIF_PUFDATA = 442,
+     H_BIF_PTYPE = 443,
+     H_BIF_IMAGECFG = 444,
+     H_BIF_PMCCONFIG = 445,
+     H_BIF_AARCH32 = 446,
+     H_BIF_BIGENDIAN = 447,
+     H_BIF_BOOTCONFIG = 448,
+     H_BIF_COPY = 449,
+     H_BIF_CORE = 450,
+     H_BIF_DELAY_HANDOFF = 451,
+     H_BIF_DELAY_LOAD = 452,
+     H_BIF_FILE = 453,
+     H_BIF_ID = 454,
+     H_BIF_IMAGE = 455,
+     H_BIF_METAHDR = 456,
+     H_BIF_NAME = 457,
+     H_BIF_PARTITION = 458,
+     H_BIF_SLR = 459,
+     H_BIF_TYPE = 460,
+     H_BIF_KEYSRCENCR = 461,
+     H_BIF_PARENTID = 462,
+     H_DPACM_ENABLE = 463,
+     H_BIF_USERKEYS = 464,
+     HVN_BIF_PCR = 465,
+     HVN_BIF_PCR_MINDEX = 466,
+     HV_BIF_IMAGESTORE = 467
    };
 
     };
@@ -469,7 +488,7 @@ namespace CO {
 #line 24 "../../parser/cmdoptions.y"
 } // CO
 /* Line 33 of lalr1.cc  */
-#line 473 "cmdoptions.tab.hpp"
+#line 492 "cmdoptions.tab.hpp"
 
 
 

commit accfdc5cf66dcaa75e7ee9cd11cf92dc558f66f8

auth: Fix GetPresign() overwriting caller buffer
GetPresign() takes the output signature buffer as an out-parameter, but
reassigned that local pointer to a freshly allocated buffer before
reading the presign file into it. This caused the caller's buffer to
never be filled in with the expected signature data.  This function is
the shared path for SPK, BH, and partition presign files, so all three
signature fields in the Authentication Certificate would come out zero
in offline/presigned-signing workflows despite bootgen logging success.

The regression was introduced by commit
efa41c591195a0ffa86528e391bcd01ff76aded0, "2026.1 feature update  (#53)".

Signed-off-by: Andrew Bradford <andrew.bradford@konsulko.com>

common/src/authentication.cpp
@@ -294,7 +294,6 @@ void AuthenticationContext::GetPresign(const std::string& presignFilename, uint8
     }
 
     LOG_TRACE("Reading presign file - %s", filename.c_str());
-    signature = new uint8_t[signatureLength];
     FILE* filePtr;
     filePtr = fopen(filename.c_str(),"rb");
     if (filePtr) 